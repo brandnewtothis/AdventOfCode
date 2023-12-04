daad36d77f86ea8728f385413d993cfb6ae32988
brandnewtothis <jflotildes@utexas.edu>
2023-12-04 00:01:20 -0600

    day_1

--- 2023/Day_1/Driver.java
+++ 2023/Day_1/Driver.java
@@ -19,14 +19,158 @@ The newly-improved calibration document consists of lines of text; each line ori
 
 public class Driver{
 	
-	public static final String[] DIGITS = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+	//don't need zero
+	public static final String[] DIGITS = {"zero","one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
 	
     public static void main(String[] args) throws Exception {
     	Parser parse = new Parser(args);
     	
-    	int total = 0;
     	ArrayList<String> input = parse.getInput();
     	
+//    	part1(input);
+    	part2(input);
+    	
+    }
+    
+    public static void part2(ArrayList<String> input) {
+    	int total = 0;
+    	for(int i = 0;i < input.size();i++) {
+    		String line = input.get(i);
+    		
+//    System.out.println(line);		
+    
+   		    boolean isFirst = true;
+   		    
+   		    char first = ' ';
+   		    char last = ' ';
+    		
+    		for(int j = 0; j < line.length();j++) {
+    		   if(Character.isDigit(line.charAt(j)) && isFirst){
+    		      isFirst = false;
+    		      first = line.charAt(j);
+    		      last = line.charAt(j);
+    		   } else if(Character.isDigit(line.charAt(j))) {
+    			  last = line.charAt(j);
+    		   }
+    		   int word = checkWord(line,j);     		  
+    		   
+    		   if(word > 0 && isFirst) {
+    			  isFirst = false;
+    		      first = (char)(word+'0');
+    		      last = (char)(word+'0');
+    		   } else if(word > 0){
+    			  last = (char)(word+'0');
+    		   }
+    		}
+    		String digit = "" + first + last;
+//    		total = Integer.valueOf(digit);
+//    		System.out.println("Word " + i + ": " + total);
+    		total += Integer.valueOf(digit);
+    	}
+    	
+    	System.out.println("Total including number words is " + total);
+    }
+    
+    public static int checkWord(String line, int index) {
+    
+    	//Null check
+    	if(index+2 >= line.length()) {
+    		return 0;
+    	}
+    	
+    	//o one 
+    	if(line.charAt(index) == 'o') {
+    		if(isMatch(line,index,1)){
+    			return 1;
+    		}
+    	}
+    	//t two, three 
+    	if(line.charAt(index) == 't') {
+    		if(isMatch(line,index,2)) {
+    			return 2;
+    		}
+    		
+    		//null check
+    		if(index+4 >= line.length()) {
+    			return 0;
+    		}
+    		
+    		if(isMatch(line,index,3)) {
+    			return 3;
+    		}
+    		
+    	}
+    	//f four, five
+    	if(line.charAt(index) == 'f') {
+    		//null check
+    		if(index+3 >= line.length()) {
+    			return 0;
+    		}
+    		
+    		if(isMatch(line,index,4)) {
+    			return 4;
+    		}
+    		
+    		if(isMatch(line,index,5)) {
+    			return 5;
+    		}
+    	}
+    	
+    	//s six, seven
+    	if(line.charAt(index) == 's') {
+    		if(isMatch(line,index,6)) {
+    			return 6;
+    		}
+    		
+    		//Null check
+    		if(index+4 >= line.length()) {
+    			return 0;
+    		}
+    		
+    		if(isMatch(line,index,7)) {
+    			return 7;
+    		}
+    		
+    	}
+    	
+   	
+    	//e eight
+    	if(line.charAt(index) == 'e') {
+    		if(index+4 >= line.length()) {
+    	      return 0;
+    	    }
+ 
+    		if(isMatch(line,index,8)) {
+    			return 8;
+    		}
+    	}
+   	
+    	//n nine
+    	if(line.charAt(index) == 'n') {
+    		if(index+3 >= line.length()) {
+    		   return 0;
+    	    }
+ 
+    		if(isMatch(line,index,9)) {
+    			return 9;
+    		}
+    	}
+    	
+    	return 0;
+    }
+    
+    //assumes out of bounds already checked
+    public static boolean isMatch(String line, int index, int digit) {
+    	for(int i = 0;i<DIGITS[digit].length();i++) {
+    	   if(DIGITS[digit].charAt(i)!=line.charAt(index+i)) {
+    		   return false;
+    	   }
+    	}
+    	return true;
+    }
+    
+    public static void part1(ArrayList<String> input) {
+    	int total = 0;
     	for(int i = 0;i < input.size();i++) {
     		String line = input.get(i);
    		    boolean isFirst = true;
@@ -48,7 +192,6 @@ public class Driver{
     	}
     	
     	System.out.println("Total is " + total);
-    	
     }
     
 }
